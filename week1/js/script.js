ea91afaf4956395d08b7c3f11664399487eb4845
adcolmenares <109199260+adcolmenares@users.noreply.github.com>
2022-07-26 17:15:30 +1000

    Add files via upload

---
+++ week1/js/script.js
@@ -0,0 +1,13 @@
+getDate = () => {
+    document.getElementById('date').innerHTML = Date();
+}
+let vue = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { title: 'assignment1', checked: true },
+            { title: 'assignment2', checked: false },
+            { title: 'assignment3', checked: false },
+        ],
+    },
+});
